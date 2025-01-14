f78e940b968e3de4319b53ec275e50a7481225cd
pravesharya <pravesharya925@gmail.com>
2025-01-14 17:05:02 +0530

    bunny

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-import * as THREE from "three";
+// import * as THREE from "three";
+import { GLTFLoader } from 'GLTFLoader';
 import { MindARThree } from "mindAR";
 
-// console.log(window.MINDAR.IMAGE);
-// const mindArThree = window.MINDAR.IMAGE.MindARThree;
-let width = window.innerWidth;
-let height = window.innerHeight;
+// let width = window.innerWidth;
+// let height = window.innerHeight;
+
+let model;
+const modelPath = "./assets/bunny/scene.gltf";
+const loader = new GLTFLoader();
 
 const mindAR = new MindARThree({
   container: document.body,
   imageTargetSrc: "./assets/nutri.mind",
 });
-console.log(mindAR);
 
 async function startMindAR() {
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  const cube = new THREE.Mesh(geometry, material);
 
-  // anchor = mindAR.createAnchor();
+  loader.load(modelPath, (gltf) => {
+    model = gltf.scene;
+    model.scale.set(0.1, 0.1, 0.1);
+  });
+
   const anchor = mindAR.addAnchor(0);
-  anchor.group.add(cube);
-  console.log(anchor);
+  anchor.group.add(model);
 
   await mindAR.start();
 
@@ -29,21 +31,5 @@ async function startMindAR() {
     renderer.render(scene, camera);
   });
 
-  //   function animate() {
-  //     requestAnimationFrame(animate);
-  //     //   cube.rotation.y += 0.01;
-  //     renderer.render(scene, camera);
-  //   }
-  //   animate();
 }
 startMindAR();
-
-// window.addEventListener("resize", () => {
-//   width = window.innerWidth;
-//   height = window.innerHeight;
-//   renderer.setSize(width, height);
-//   camera.aspect = width / height;
-//   camera.updateProjectionMatrix();
-// //   BG.width = width;
-//   BG.height = height;
-// });
