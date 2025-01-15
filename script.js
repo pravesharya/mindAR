305a022ffc623e0f9b914c738c56a3d6e873d946
pravesharya <pravesharya925@gmail.com>
2025-01-15 15:11:17 +0530

    bunyy working

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-// import * as THREE from "three";
-import { GLTFLoader } from 'GLTFLoader';
+import * as THREE from "three";
+import { GLTFLoader } from "GLTFLoader";
 import { MindARThree } from "mindAR";
-
-// let width = window.innerWidth;
-// let height = window.innerHeight;
 
 let model;
 const modelPath = "./assets/bunny/scene.gltf";
 const loader = new GLTFLoader();
 
-const mindAR = new MindARThree({
-  container: document.body,
-  imageTargetSrc: "./assets/nutri.mind",
-});
-
 async function startMindAR() {
+
+  const mindAR = new MindARThree({
+    container: document.body,
+    imageTargetSrc: "./assets/targetNutri.mind",
+  });
+  const anchor = mindAR.addAnchor(0);
+  const { renderer, camera, scene } = mindAR;
+
+  const light = new THREE.HemisphereLight(0xffffff, 10);
+  scene.add(light);
 
   loader.load(modelPath, (gltf) => {
     model = gltf.scene;
-    model.scale.set(0.5, 0.5, 0.5);
+    model.scale.set(0.25, 0.25, 0.25);
+    model.position.set(0, 0, -1);
+    model.rotation.set(90, 0, 0);
+    anchor.group.add(model);
   });
-
-  const anchor = mindAR.addAnchor(0);
-  anchor.group.add(model);
 
   await mindAR.start();
 
-  const { renderer, camera, scene } = mindAR;
   renderer.setAnimationLoop(() => {
     renderer.render(scene, camera);
   });
-
 }
 startMindAR();
